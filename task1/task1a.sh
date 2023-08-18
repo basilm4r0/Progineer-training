0d06cbda821c87e40c60065393d2add701260ea0
Basil Mari <basilm4r0@gmail.com>
2023-08-18 22:15:20 +0300

    Add first script of task one

---
+++ task1/task1a.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+while getopts ":h" opt; do
+    case $opt in
+        h)
+            printf "Description:\n"
+            printf "\tThis script creates a \"tmp\" with 5 directories in it in the home directory, and creates two text files within each subdirectory.\n\n"
+            printf "Usage:\n"
+            printf "\ttask1a.sh [-h]\n"
+            printf "Options:\n"
+            printf "\t-h: Displays this help message and exits.\n"
+            exit 0
+            ;;
+        ?)
+            printf "Invalid option: -$OPTARG\n" >&2
+            exit 1
+            ;;
+    esac
+done
+
+rm -r ~/tmp
+
+for i in 1 2 3 4 5
+do
+
+    mkdir -p ~/tmp/training_project${i}
+	cd ~/tmp/training_project${i} || exit
+	touch module_${i}_a.txt module_${i}_b.txt
+	printf "Hello Basil Mari welcome to file ${i}.A" > module_${i}_a.txt
+	printf "Hello Basil Mari welcome to file ${i}.B" > module_${i}_b.txt
+
+done
+
+exit 0
